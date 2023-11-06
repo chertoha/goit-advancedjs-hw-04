7fb11b177b158f235c04e98b03e98aa14711aaa8
chertoha <a.chertok@akatech.com.ua>
2023-11-06 09:45:39 +0000

    Deploying to gh-pages from @ chertoha/goit-advancedjs-hw-04@9d641a85d8445ac649bf88f28fd30851c5b76453 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,22 +1,21 @@
-import{a as i,S as c}from"./assets/vendor-7ef20b6d.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const l="40477492-fb7a942987769cd06fc4fed72",p="https://pixabay.com/api/",f=i.create({baseURL:p,params:{key:l,image_type:"photo",orientation:"horizontal",safesearch:!0}}),u=async s=>(await f.get("",{params:{q:s}})).data,d=document.querySelector(".search"),m=document.querySelector(".gallery"),h=new c(".gallery a",{showCounter:!1});d.addEventListener("submit",s=>{s.preventDefault();const o=s.target.elements.searchQuery.value.trim();u(o).then(r=>{console.log("",r),y(r.hits),h.refresh()}).catch(r=>{console.log(r)})});function y(s){const o=s.map(g).join("");m.insertAdjacentHTML("afterbegin",o)}function g({largeImageURL:s,tags:o,likes:r,views:a,comments:e,downloads:t,webformatURL:n}){return`
+import{a as i,S as c}from"./assets/vendor-7ef20b6d.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const l="40477492-fb7a942987769cd06fc4fed72",p="https://pixabay.com/api/",f=i.create({baseURL:p,params:{key:l,image_type:"photo",orientation:"horizontal",safesearch:!0}}),u=async r=>(await f.get("",{params:{q:r}})).data;function d(r){return r.map(m).join("")}function m({largeImageURL:r,tags:o,likes:s,views:a,comments:e,downloads:t,webformatURL:n}){return`
   <div class="photo-card">
-    <a class="photo-card__link" href="${s}">
+    <a class="photo-card__link" href="${r}">
         <img class="photo-card__img" src="${n}" alt="${o}" loading="lazy" />
-    </a>
-    
+    </a>    
     <div class="info">
         <p class="info-item">
-            <b>Likes <span>${r}</span></b>
-        </p>
-            <p class="info-item">
-        <b>Views <span>${a}</span></b>
+            <b>Likes</b><span>${s}</span>
         </p>
         <p class="info-item">
-            <b>Comments <span>${e}</span></b>
+            <b>Views</b><span>${a}</span>
         </p>
         <p class="info-item">
-            <b>Downloads <span>${t}</span></b>
+            <b>Comments</b><span>${e}</span>
+        </p>
+        <p class="info-item">
+            <b>Downloads</b><span>${t}</span>
         </p>
     </div>
-</div>`}
+</div>`}const h=document.querySelector(".search"),y=document.querySelector(".gallery"),g=new c(".gallery a",{showCounter:!1});h.addEventListener("submit",r=>{r.preventDefault(),r.target.elements.searchQuery.value.trim(),u("cat").then(o=>{console.log("",o);const s=d(o.hits);y.insertAdjacentHTML("afterbegin",s),g.refresh()}).catch(o=>{console.log(o)})});
 //# sourceMappingURL=commonHelpers.js.map
